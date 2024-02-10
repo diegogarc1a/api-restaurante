8ad597c4c4373aee31f6ba37dd6fb215088387b5
[Diego] <[diegoemanuel134@gmail.com>
2024-02-10 10:55:17 -0600

    commit sin track

--- src/main/java/com/sistema/apirestaurante/repositories/DetalleVentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sistema.apirestaurante.repositories;
-
-import com.sistema.apirestaurante.entidades.DetalleVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
-}
--- src/main/java/com/sistema/apirestaurante/repositories/VentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sistema.apirestaurante.repositories;
-
-import com.sistema.apirestaurante.entidades.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VentaRepository extends JpaRepository<Venta, Long> {
-}
