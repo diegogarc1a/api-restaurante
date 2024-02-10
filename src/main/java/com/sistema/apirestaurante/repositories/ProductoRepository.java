4f3a8af4b031b0a5b8d83387d52d3d6e13db7d57
[Diego] <[diegoemanuel134@gmail.com>
2024-02-10 10:53:00 -0600

    fixed untracked files

--- src/main/java/com/sistema/apirestaurante/repositories/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sistema.apirestaurante.repositories;
-
-import com.sistema.apirestaurante.entidades.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-
-}
